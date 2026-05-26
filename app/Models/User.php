ad7a17af77d1c9afcab14ee175dc2e84aee6d52e
Sourish Mondal <sourishmondal@Sourishs-MacBook-Air.local>
2026-05-26 09:22:12 +0530

    Fix CI test failures

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,10 +11,11 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
 
-#[Fillable(['name', 'email', 'password'])]
-#[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
+    protected $fillable = ['name', 'email', 'password'];
+    
+    protected $hidden = ['password', 'remember_token'];
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable, HasRoles;
 
