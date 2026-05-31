b6ff85865ad7f069822d27dedb8d70fce38a24f0
Sourish Mondal <sourishmondal@Sourishs-MacBook-Air.local>
2026-05-31 18:00:30 +0530

    chore: add comprehensive platform data seeder with ai forecasting

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,7 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
         $this->call([
             RolesAndPermissionsSeeder::class,
-            RealisticDataSeeder::class,
+            PlatformDataSeeder::class,
         ]);
     }
 }
